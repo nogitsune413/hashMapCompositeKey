5335a767ddc63cad9cf2814bb7daed7efd1772fa
nogitsune413 <nakamuray4@gmail.com>
2017-02-11 19:42:12 +0900

    Genericsの型推論を使うように修正。

--- src/hoge/TestMain.java
+++ src/hoge/TestMain.java
@@ -11,13 +11,13 @@ public class TestMain {
 	public static void main(String[] args) {
 
 		// テストデータ
-		List<SalesRecord> salesRecords = new ArrayList<SalesRecord>();
+		List<SalesRecord> salesRecords = new ArrayList<>();
 		salesRecords.add(new SalesRecord(1,LocalDate.of(2017,1,1),100));
 		salesRecords.add(new SalesRecord(2,LocalDate.of(2017,1,2),200));
 		salesRecords.add(new SalesRecord(3,LocalDate.of(2017,1,3),300));
 
 		// HashMapを生成
-		Map<CompositeKey,SalesRecord> map = new HashMap<CompositeKey,SalesRecord>();
+		Map<CompositeKey,SalesRecord> map = new HashMap<>();
 
 		for (SalesRecord record : salesRecords) {
 			map.put(new CompositeKey(record.getStoreNo(), record.getSalesDate()), record);
